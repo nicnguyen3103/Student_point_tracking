f7c03f393a11893e8b207c931c78c91bc12be183
Nguyen <nguyenhoangnguyen96nt@gmail.com>
2020-02-16 10:40:00 +0000

    form.js get all the data

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request,jsonify
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-from wtforms import StringField, IntegerField, SelectField, SubmitField, DateField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from flask_wtf import FlaskForm
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, String, Integer, Text, UnicodeText, ForeignKey, Float, desc, func
+import requests
+import json
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'somekey'
 db = SQLAlchemy(app)
+
+# Other static variable: 
+ATTENDANCE_POINT = 10
+LATE_POINT = -10
 
 class Student(db.Model):
     __tablename__ = 'student'
@@ -38,46 +40,45 @@ class Logs(db.Model):
     date = db.Column(db.Date, nullable=False, default=datetime.utcnow)
     total_point = db.Column(db.Integer, nullable=False)
 
-# class Point(db.Model):
-#     __tablename__= 'point'
+class Point(db.Model):
+    __tablename__= 'point'
 
-#     student_id = db.Column(db.Integer, ForeignKey('logs.student_id'), primary_key=True)
-#     log_id = db.Column(db.Integer, ForeignKey('logs.log_id'), primary_key=True)
-#     total_point = db.Column(db.Integer, nullable=False)
+    student_id = db.Column(db.Integer, ForeignKey('logs.student_id'), primary_key=True)
+    log_id = db.Column(db.Integer, ForeignKey('logs.log_id'), primary_key=True)
+    total_point = db.Column(db.Integer, nullable=False)
 
-class PointForm(FlaskForm):
-    point = IntegerField('Add/Remove Point')
-    activities = StringField('Activities')
-    checkin = BooleanField('Check-in')
-    submit = SubmitField('Submit')
+# class PointForm(FlaskForm):
+#     point = IntegerField('Add/Remove Point')
+#     activities = StringField('Activities')
+#     checkin = BooleanField('Check-in')
+#     submit = SubmitField('Submit')
+
+
+
 
 @app.route('/home', methods=['GET', 'POST'])
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    form = PointForm()
     all_students = []
-    # query = '''select l.student_id, max(l.total_point), l.date, s.name
-    # from logs l join student s on l.student_id = s.student_id
-    # GROUP by l.student_id '''
-    # test = db.session.execute(query).
-    # print(test)
     all_students = db.session.query(Logs.student_id, Student.name, func.max(Logs.total_point), Logs.date).join(Student).group_by(Logs.student_id).order_by(desc(Logs.total_point)).all()
-    # all_students = db.session.query(Student, Logs).join(Student).all()
     if request.method == 'GET':
-        return render_template('home.html', all_students=all_students, form=form)
-    elif form.validate_on_submit() and request.method == 'POST':
-        student_id = int(request.form['student_id'])
-        print(student_id)
-        print(f'mycheckbox {student_id}')
-        print(form.data.get(f'mycheckbox {student_id}'))
-        latest_point = Logs.query.with_entities(Logs.total_point).filter_by(student_id=student_id).order_by(desc(Logs.log_id)).first()[0]
-        new_log = Logs(student_id=student_id, activities=form.activities.data, point_gain=form.point.data, total_point=latest_point + int(form.point.data) )
-        db.session.add(new_log)
-        db.session.commit()
+        return render_template('home.html', all_students=all_students)
+    elif request.method == 'POST':
+        student_data = request.get_json()
+        for data in student_data:
+            latest_point = Logs.query.with_entities(Logs.total_point).filter_by(student_id=data['id']).order_by(desc(Logs.log_id)).first()[0]
+            print(latest_point)
+            if data['checkin'] == True:
+                attendance = Logs(student_id=data['id'], activities='Attendance', point_gain=ATTENDANCE_POINT, total_point=latest_point + int(ATTENDANCE_POINT) )
+                db.session.add(attendance)
+                db.session.commit()
+            if data['activity']!='' or data['point'] != '': 
+                new_log = Logs(student_id=data['id'], activities=data['activity'], point_gain=int(data['point']), total_point=latest_point + int(data['point']) )
+                db.session.add(new_log)
+                db.session.commit()
+            
         return redirect(url_for('home'))
-    else: 
-        return redirect(url_for('home'))
-
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/form.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+    var submitButton = document.getElementById("submit")
+    submitButton.addEventListener("click",ajax_form_submit);
+    
+    function ajax_form_submit(){
+        console.log("testing ajax");
+        student_rows = document.getElementsByClassName("student_row");
+        var arrayLength = student_rows.length;
+        student_datas = [];
+        for (var i = 0; i < arrayLength; i++) {
+            var id=document.getElementsByClassName("student_id")[i].value;
+            var activity=document.getElementsByClassName("student_activity")[i].value;
+            var point =document.getElementsByClassName("student_point")[i].value;
+            var checkin = document.getElementsByClassName("student_checkin")[i].checked;
+            student_datas.push(
+                {"id": id,
+                "activity":activity,
+                "point": point,
+                "checkin": checkin
+                }
+            )
+        }
+        console.log(student_datas);
+        fetch('/', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(student_datas)
+            }).then(res=>res.json())
+            .then(res => console.log(res));
+        
+    }
+
+})
+
